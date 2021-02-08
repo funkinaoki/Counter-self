3f3ee8000aff4b7434c64e1c80d9f4e3af8ed697
Naoki Yahata <funkinaoki@gmail.com>
2021-02-08 14:10:27 +0900

    Initial Commit

---
+++ Counter-self/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Counter-self
+//
+//  Created by 八幡尚希 on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var number: Int = 0
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+    }
+
+
+}
+
